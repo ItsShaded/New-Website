05d4682d7fae4f055acfe3fd297e5d280dd75cce
Shaded <72438879+ItsShaded@users.noreply.github.com>
2022-03-24 13:20:47 +0530

    Add files via upload

---
+++ js/info.js
@@ -0,0 +1,10 @@
+/* Script to update some time-related fields. */
+
+document.addEventListener("DOMContentLoaded", () => {
+    let year = (new Date()).getFullYear();
+    let age = document.getElementById("age");
+  
+    if (age != undefined) {
+      age.innerHTML = year - 2006;
+    }
+  });
